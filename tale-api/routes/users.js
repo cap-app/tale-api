b9104928ddd1c9e55a4d3cfb1a3fde12633ae132
Sebastian Markgraf <sebastian.markgraf@diaratio.de>
2017-10-01 00:06:05 +0200

    Fixed var to let

--- tale-api/routes/users.js
+++ tale-api/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var User = require('../models/User.js');
+var User = mongoose.model('User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
--- tests/GroupEtaServiceTest.js
+++ tests/GroupEtaServiceTest.js
@@ -1,13 +1,13 @@
-var updateUserService = require('../tale-api/services/updateUserEta.js');
+const updateUserService = require('../tale-api/services/updateUserEta.js');
 
 function testGroupBasic() {
-    var person1 = {userName: "John1",
+    let person1 = {userName: "John1",
         location: "49.0044918,8.3899858",
         transitMode: "walking"};
-    var person2 = {userName: "Detleff",
+    let person2 = {userName: "Detleff",
         location: "48.974532, 8.439119",
         transitMode: "driving"};
-    var group = {groupName: "SWAG MEET",
+    let group = {groupName: "SWAG MEET",
         address: "48.9875101,8.4204697",
         etaLast: 800,
         users: [person1, person2],};
