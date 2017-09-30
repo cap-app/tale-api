5836162360a477ab5802eae576d85ddb7d9ea4b9
Sebastian Markgraf <sebastian.markgraf@diaratio.de>
2017-09-30 23:58:00 +0200

    Use of constant for Traffic Model

--- tale-api/services/updateUserEta.js
+++ tale-api/services/updateUserEta.js
@@ -2,12 +2,13 @@ const googleMapsClient = require('../node_modules/@google/maps').createClient({
     key: 'AIzaSyAjp_mHqTsLdQ0pNwlpeTqPnFaX_nKOqds',
     Promise: Promise
 });
-var Group = require('mongoose').model('Group');
+const Group = require('mongoose').model('Group');
+const TRAFFIC_MODEL = 'best_guess';
 
 
 async function updateUserEta(user) {
-    var groups = Group.findAllUserGroups(user);
-    var destination = [];
+    let groups = Group.findAllUserGroups(user);
+    let destination = [];
     groups.forEach(function(group) {
       destination.push(group.address);
     });
@@ -19,14 +20,14 @@ async function updateUserEta(user) {
             destination
         ],
         mode: user.transitMode,
-        traffic_model: 'best_guess'
+        traffic_model: TRAFFIC_MODEL
 
     }).asPromise().then(function(response) {
         if (response.status === 'OK') {
             console.log("Received correct response.");
             console.log("Start updating group Eta");
-            for (var i = 0; i < response.rows[0].elements.length; i++) {
-                var eta = response.rows[0].elements[i].duration.value;
+            for (let i = 0; i < response.rows[0].elements.length; i++) {
+                let eta = response.rows[0].elements[i].duration.value;
                 console.log("Updating Group: " + i);
                 updateLastEta(eta, groups[i]);
             }
@@ -42,13 +43,6 @@ async function updateUserEta(user) {
         group.etaLast = eta;
         console.log("User was slowest.");
     }
-}
-
-function findGroups(user) {
-
-
-
-    return groups;
 }
 
 
