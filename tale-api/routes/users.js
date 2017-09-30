fc5d5f72e7a989d7ac23ec95a21b6cb9998f67d3
Sebastian Markgraf <sebastian.markgraf@diaratio.de>
2017-10-01 00:34:58 +0200

    Added put of user for location

--- tale-api/routes/users.js
+++ tale-api/routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var mongoose = require('mongoose');
 var User = require('../models/User.js');
+const userUpdateService = require('../services/updateUserEta');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -17,6 +18,19 @@ router.post('/', function(req, res, next) {
     User.create(req.body, function (err, post) {
         if (err) return next(err);
         res.json(post);
+    });
+});
+
+router.put('/', function(req, res, next) {
+    User.findOneAndUpdate({'_id': req.query.id}, {$set: {location: req.query.location}}, function(err, result) {
+        if (err) {
+            console.log('Error updating location: ' + err);
+            res.send({'error':'An error has occurred'});
+        } else {
+            console.log("Updating location successful");
+            userUpdateService.updateUserEta(this);
+            res.json(result);
+        }
     });
 });
 
