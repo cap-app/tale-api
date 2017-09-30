d82c3efb989cf3f6b9c5da54ec3be3fc6d4ef201
Oliver Dietz <olli.dietz@web.de>
2017-09-30 20:23:24 +0200

    Mongo db test

--- tale-api/app.js
+++ tale-api/app.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 // Use native Node promises
 mongoose.Promise = global.Promise;
 // connect to MongoDB
-mongoose.connect('mongodb://tale_mongoadmin:ooChequ8th@localhost:21422/tale-app', {
+mongoose.connect('mongodb://tale_mongoadmin:ooChequ8th@127.0.0.1:21422/tale-app', {
     useMongoClient: true
     /* other options */
 });
