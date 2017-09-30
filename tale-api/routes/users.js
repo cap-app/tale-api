eaab62be926a13b9529761f4bf445a69850f8d19
Sebastian Markgraf <sebastian.markgraf@diaratio.de>
2017-10-01 01:57:15 +0200

    Changed to _id

--- tale-api/routes/users.js
+++ tale-api/routes/users.js
@@ -23,9 +23,9 @@ router.post('/', function(req, res, next) {
 
 router.put('/', function(req, res, next) {
     console.log(req.body);
-    var id = req.params.id;
+    var id = req.params._id;
 
-    User.findByIdAndUpdate(id, req, function(err, user) {
+    User.findByIdAndUpdate(id, req.body, function(err, user) {
         if (err) {
             console.log('Error updating location: ' + err);
             res.send({'error':'An error has occurred'});
