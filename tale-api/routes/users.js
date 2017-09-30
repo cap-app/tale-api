a8be14d8a90cbf6dea8894a8ebbec3f6202fc8e7
Sebastian Markgraf <sebastian.markgraf@diaratio.de>
2017-10-01 00:13:24 +0200

    Hopefully User Bugfix in users.js ERROR: User Schema not registered

--- tale-api/routes/users.js
+++ tale-api/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var User = mongoose.model('User');
+var User = require('../models/User.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
