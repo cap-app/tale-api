af7a9b796ad8042706644c1ff15f4400cbedd6ed
Sebastian Markgraf <sebastian.markgraf@diaratio.de>
2017-10-01 02:22:09 +0200

    Changed array to User

--- tale-api/models/Group.js
+++ tale-api/models/Group.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
+var User = require('User');
 
 // create project schema
 var GroupSchema = new mongoose.Schema({
     groupName: String,
     address: String,
     etaLast: Number,
-    users: Array
+    users: [User]
 });
 
 
