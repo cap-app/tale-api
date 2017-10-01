f65ca11a18ec2751fd5b872fc53ce10614376d2c
Sebastian Markgraf <sebastian.markgraf@diaratio.de>
2017-10-01 02:43:49 +0200

    More Async

--- tale-api/services/updateUserEta.js
+++ tale-api/services/updateUserEta.js
@@ -8,35 +8,37 @@ const TRAFFIC_MODEL = 'best_guess';
 
 
 async function updateUserEta(user) {
-    let groups = await Group.findAllUserGroups(user);
-    console.log("Groups: " + groups);
-    let destination = [];
-    groups.forEach(function(group) {
-      destination.push(group.address);
-    });
-    await googleMapsClient.distanceMatrix({
-        origins: [
-            user.location
-        ],
-        destinations: [
-            destination
-        ],
-        mode: user.transitMode,
-        traffic_model: TRAFFIC_MODEL
+    Group.findAllUserGroups(user).then(function(groups) {
+        console.log("Groups: " + groups);
+        let destination = [];
+        groups.forEach(function (group) {
+            destination.push(group.address);
+        });
+    }).then(function() {
+        googleMapsClient.distanceMatrix({
+            origins: [
+                user.location
+            ],
+            destinations: [
+                destination
+            ],
+            mode: user.transitMode,
+            traffic_model: TRAFFIC_MODEL
 
-    }).asPromise().then(function(response) {
-        if (response.status === 'OK') {
-            console.log("Received correct response.");
-            console.log("Start updating group Eta");
-            for (let i = 0; i < response.rows[0].elements.length; i++) {
-                let eta = response.rows[0].elements[i].duration.value;
-                console.log("Updating Group: " + i);
-                updateLastEta(eta, groups[i]);
+        }).asPromise().then(function(response) {
+            if (response.status === 'OK') {
+                console.log("Received correct response.");
+                console.log("Start updating group Eta");
+                for (let i = 0; i < response.rows[0].elements.length; i++) {
+                    let eta = response.rows[0].elements[i].duration.value;
+                    console.log("Updating Group: " + i);
+                    updateLastEta(eta, groups[i]);
+                }
+                console.log("Update finished");
             }
-            console.log("Update finished");
-        }
-    }, function() {
-        console.log("API Call screwed up.")
+        }, function() {
+            console.log("API Call screwed up.")
+        })
     });
 }
 
